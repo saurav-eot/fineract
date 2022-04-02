f53484b52475aab3d79e98b37a05db9a701fe6bd
Vic Romero <victor.romero@fintecheando.mx>
2022-04-02 02:25:05 -0600

    Add JdbcURL Builder for GCP

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/domain/FineractPlatformTenantConnection.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/domain/FineractPlatformTenantConnection.java
@@ -195,6 +195,16 @@ public class FineractPlatformTenantConnection {
         return sb.toString();
     }
 
+    public static String toJdbcUrlGCP(String protocol, String db, String parameters) {
+        StringBuilder sb = new StringBuilder(protocol).append(":///").append(db);
+
+        if (!StringUtils.isEmpty(parameters)) {
+            sb.append('?').append(parameters);
+        }
+
+        return sb.toString();
+    }
+
     public static String toProtocol(DataSource dataSource) {
         try {
             String url = dataSource.getConnection().getMetaData().getURL();
