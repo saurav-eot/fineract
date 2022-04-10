83cf2724f25eaa111e66583d5ee53cc00fc70810
Vic Romero <victor.romero@fintecheando.mx>
2022-04-10 02:20:10 -0500

    Support for Pentaho reports

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/report/service/PentahoReportingProcessServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/report/service/PentahoReportingProcessServiceImpl.java
@@ -52,6 +52,7 @@ import org.slf4j.LoggerFactory;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
